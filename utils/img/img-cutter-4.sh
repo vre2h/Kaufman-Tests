ca919eaef29f0a8865867a45a602ab6ac9a8070a
vrezh <hovanisyan.vrezh@gmail.com>
2019-03-31 16:00:26 +0400

    feat: add bash scripts for cutting images

---
+++ utils/img/img-cutter-4.sh
@@ -0,0 +1,4 @@
+convert ./$1.jpg -crop 1130x1000+362+88 ./output/$2_1.jpg
+convert ./$1.jpg -crop 1130x1000+1568+88 ./output/$2_2.jpg
+convert ./$1.jpg -crop 1130x1000+362+1197 ./output/$2_3.jpg
+convert ./$1.jpg -crop 1130x1000+1568+1197 ./output/$2_4.jpg
---
+++ utils/img/img-cutter-5.sh
@@ -0,0 +1,5 @@
+convert ./$1.jpg -crop 820x800+192+211 ./output/$2_1.jpg
+convert ./$1.jpg -crop 820x800+1086+211 ./output/$2_2.jpg
+convert ./$1.jpg -crop 820x800+1990+211 ./output/$2_3.jpg
+convert ./$1.jpg -crop 820x800+640+1182 ./output/$2_4.jpg
+convert ./$1.jpg -crop 820x800+1542+1182 ./output/$2_5.jpg
