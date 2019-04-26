24135c03dc6e109784ea391758289deb476ecb1c
vrezh <hovanisyan.vrezh@gmail.com>
2019-04-27 01:35:14 +0400

    upd image container folder

---
+++ src/tests/test-2/test-2.js
@@ -0,0 +1,326 @@
+export default {
+  0: {
+    id: 1,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/1_1.jpg"),
+      require("../../img/PatternReasoning/1_2.jpg"),
+      require("../../img/PatternReasoning/1_3.jpg"),
+      require("../../img/PatternReasoning/1_4.jpg"),
+      require("../../img/PatternReasoning/1_5.jpg")
+    ],
+    answer: "4"
+  },
+  1: {
+    id: 2,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/2_1.jpg"),
+      require("../../img/PatternReasoning/2_1.jpg"),
+      require("../../img/PatternReasoning/2_2.jpg"),
+      require("../../img/PatternReasoning/2_1.jpg")
+    ],
+    answer: "3"
+  },
+  2: {
+    id: 3,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/3_4.jpg"),
+      require("../../img/PatternReasoning/3_1.jpg"),
+      require("../../img/PatternReasoning/3_2.jpg"),
+      require("../../img/PatternReasoning/3_3.jpg")
+    ],
+    answer: "2"
+  },
+  3: {
+    id: 4,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/4_4.jpg"),
+      require("../../img/PatternReasoning/4_1.jpg"),
+      require("../../img/PatternReasoning/4_2.jpg"),
+      require("../../img/PatternReasoning/4_3.jpg")
+    ],
+    answer: "3"
+  },
+  4: {
+    id: 5,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/5_4.jpg"),
+      require("../../img/PatternReasoning/5_1.jpg"),
+      require("../../img/PatternReasoning/5_2.jpg"),
+      require("../../img/PatternReasoning/5_3.jpg")
+    ],
+    answer: "4"
+  },
+  5: {
+    id: 6,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/6_4.jpg"),
+      require("../../img/PatternReasoning/6_1.jpg"),
+      require("../../img/PatternReasoning/6_2.jpg"),
+      require("../../img/PatternReasoning/6_3.jpg")
+    ],
+    answer: "4"
+  },
+  6: {
+    id: 7,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/7_4.jpg"),
+      require("../../img/PatternReasoning/7_1.jpg"),
+      require("../../img/PatternReasoning/7_2.jpg"),
+      require("../../img/PatternReasoning/7_3.jpg")
+    ],
+    answer: "2"
+  },
+  7: {
+    id: 8,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/8_4.jpg"),
+      require("../../img/PatternReasoning/8_1.jpg"),
+      require("../../img/PatternReasoning/8_2.jpg"),
+      require("../../img/PatternReasoning/8_3.jpg")
+    ],
+    answer: "1"
+  },
+  8: {
+    id: 9,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/9_4.jpg"),
+      require("../../img/PatternReasoning/9_1.jpg"),
+      require("../../img/PatternReasoning/9_2.jpg"),
+      require("../../img/PatternReasoning/9_3.jpg")
+    ],
+    answer: "1"
+  },
+  9: {
+    id: 10,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/10_4.jpg"),
+      require("../../img/PatternReasoning/10_1.jpg"),
+      require("../../img/PatternReasoning/10_2.jpg"),
+      require("../../img/PatternReasoning/10_3.jpg")
+    ],
+    answer: "3"
+  },
+  10: {
+    id: 11,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/11_4.jpg"),
+      require("../../img/PatternReasoning/11_1.jpg"),
+      require("../../img/PatternReasoning/11_2.jpg"),
+      require("../../img/PatternReasoning/11_3.jpg")
+    ],
+    answer: "3"
+  },
+  11: {
+    id: 12,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/12_4.jpg"),
+      require("../../img/PatternReasoning/12_1.jpg"),
+      require("../../img/PatternReasoning/12_2.jpg"),
+      require("../../img/PatternReasoning/12_3.jpg")
+    ],
+    answer: "2"
+  },
+  12: {
+    id: 13,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/13_4.jpg"),
+      require("../../img/PatternReasoning/13_1.jpg"),
+      require("../../img/PatternReasoning/13_2.jpg"),
+      require("../../img/PatternReasoning/13_3.jpg")
+    ],
+    answer: "1"
+  },
+  13: {
+    id: 14,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/14_4.jpg"),
+      require("../../img/PatternReasoning/14_1.jpg"),
+      require("../../img/PatternReasoning/14_2.jpg"),
+      require("../../img/PatternReasoning/14_3.jpg")
+    ],
+    answer: "2"
+  },
+  14: {
+    id: 15,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/15_4.jpg"),
+      require("../../img/PatternReasoning/15_1.jpg"),
+      require("../../img/PatternReasoning/15_2.jpg"),
+      require("../../img/PatternReasoning/15_3.jpg"),
+      require("../../img/PatternReasoning/15_5.jpg")
+    ],
+    answer: "5"
+  },
+  15: {
+    id: 16,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/16_4.jpg"),
+      require("../../img/PatternReasoning/16_1.jpg"),
+      require("../../img/PatternReasoning/16_2.jpg"),
+      require("../../img/PatternReasoning/16_3.jpg"),
+      require("../../img/PatternReasoning/16_5.jpg")
+    ],
+    answer: "2"
+  },
+  16: {
+    id: 17,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/17_4.jpg"),
+      require("../../img/PatternReasoning/17_1.jpg"),
+      require("../../img/PatternReasoning/17_2.jpg"),
+      require("../../img/PatternReasoning/17_3.jpg"),
+      require("../../img/PatternReasoning/17_5.jpg")
+    ],
+    answer: "4"
+  },
+  17: {
+    id: 18,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/18_4.jpg"),
+      require("../../img/PatternReasoning/18_1.jpg"),
+      require("../../img/PatternReasoning/18_2.jpg"),
+      require("../../img/PatternReasoning/18_3.jpg"),
+      require("../../img/PatternReasoning/18_5.jpg")
+    ],
+    answer: "5"
+  },
+  18: {
+    id: 19,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/19_4.jpg"),
+      require("../../img/PatternReasoning/19_1.jpg"),
+      require("../../img/PatternReasoning/19_2.jpg"),
+      require("../../img/PatternReasoning/19_3.jpg"),
+      require("../../img/PatternReasoning/19_5.jpg")
+    ],
+    answer: "2"
+  },
+  19: {
+    id: 20,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/20_4.jpg"),
+      require("../../img/PatternReasoning/20_1.jpg"),
+      require("../../img/PatternReasoning/20_2.jpg"),
+      require("../../img/PatternReasoning/20_3.jpg"),
+      require("../../img/PatternReasoning/20_5.jpg")
+    ],
+    answer: "1"
+  },
+  20: {
+    id: 21,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/21_4.jpg"),
+      require("../../img/PatternReasoning/21_1.jpg"),
+      require("../../img/PatternReasoning/21_2.jpg"),
+      require("../../img/PatternReasoning/21_3.jpg"),
+      require("../../img/PatternReasoning/21_5.jpg")
+    ],
+    answer: "2"
+  },
+  21: {
+    id: 22,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/22_4.jpg"),
+      require("../../img/PatternReasoning/22_1.jpg"),
+      require("../../img/PatternReasoning/22_2.jpg"),
+      require("../../img/PatternReasoning/22_3.jpg"),
+      require("../../img/PatternReasoning/22_5.jpg")
+    ],
+    answer: "3"
+  },
+  22: {
+    id: 23,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/23_4.jpg"),
+      require("../../img/PatternReasoning/23_1.jpg"),
+      require("../../img/PatternReasoning/23_2.jpg"),
+      require("../../img/PatternReasoning/23_3.jpg"),
+      require("../../img/PatternReasoning/23_5.jpg")
+    ],
+    answer: "4"
+  },
+  23: {
+    id: 24,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/24_4.jpg"),
+      require("../../img/PatternReasoning/24_1.jpg"),
+      require("../../img/PatternReasoning/24_2.jpg"),
+      require("../../img/PatternReasoning/24_3.jpg"),
+      require("../../img/PatternReasoning/24_5.jpg")
+    ],
+    answer: "1"
+  },
+  24: {
+    id: 25,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/25_4.jpg"),
+      require("../../img/PatternReasoning/25_1.jpg"),
+      require("../../img/PatternReasoning/25_2.jpg"),
+      require("../../img/PatternReasoning/25_3.jpg"),
+      require("../../img/PatternReasoning/25_5.jpg")
+    ],
+    answer: "2"
+  },
+  25: {
+    id: 26,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/26_4.jpg"),
+      require("../../img/PatternReasoning/26_1.jpg"),
+      require("../../img/PatternReasoning/26_2.jpg"),
+      require("../../img/PatternReasoning/26_3.jpg"),
+      require("../../img/PatternReasoning/26_5.jpg")
+    ],
+    answer: "4"
+  },
+  26: {
+    id: 27,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/27_4.jpg"),
+      require("../../img/PatternReasoning/27_1.jpg"),
+      require("../../img/PatternReasoning/27_2.jpg"),
+      require("../../img/PatternReasoning/27_3.jpg"),
+      require("../../img/PatternReasoning/27_5.jpg")
+    ],
+    answer: "4"
+  },
+  27: {
+    id: 28,
+    description: "Test 2",
+    images: [
+      require("../../img/PatternReasoning/28_4.jpg"),
+      require("../../img/PatternReasoning/28_1.jpg"),
+      require("../../img/PatternReasoning/28_2.jpg"),
+      require("../../img/PatternReasoning/28_3.jpg"),
+      require("../../img/PatternReasoning/28_5.jpg")
+    ],
+    answer: "1"
+  },
+  length: 28
+};
